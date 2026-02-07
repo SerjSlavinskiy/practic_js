7590939c226dbf14ddcdfa72771c9f0d93abb4e5
Sergiy <resritesiroj@gmail.com>
2026-02-07 02:59:11 +0200

    secong version

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один з останніх фільмів, який Ви подивились?', ''),
-      b = prompt('Як Ви його оціните?', ''),
-      c = prompt('Один з останніх фільмів, який Ви подивились?', ''),
-      d = prompt('Як Ви його оціните?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+ for (let i = 0; i < 2; i++) {
+   const a = prompt('Один з останніх фільмів, який Ви подивились?', ''),
+         b = prompt('Як Ви його оціните?', '')
+         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+             personalMovieDB.movies[a] = b;
+             console.log('done');
+         } else {
+             console.log('error');
+             i--;
+         }
+ }
+
+if (personalMovieDB.count < 10) {
+    console.log("Ви дивились замало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Ви хороший глядач");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Та Ви кіноман");
+} else {
+    console.log("Сталась помилка");
+}
 
 console.log(personalMovieDB);
